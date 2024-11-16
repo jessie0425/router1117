59b4a1c4ad4197800ca4a05b03104393fed17076
石喬昕 <jessieshi0425@gmail.com>
2024-11-17 00:22:21 +0800

    [ADD]新增gh-pages，用於佈版

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/router1117/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
